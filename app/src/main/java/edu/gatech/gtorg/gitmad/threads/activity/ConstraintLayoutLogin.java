76c7e378ee3467e9994275015ca9c29fa2db052c
Shannor Trotty <shannor.a.trotty@gmail.com>
2017-01-24 21:58:32 -0500

    added layouts for login

---
+++ app/src/main/java/edu/gatech/gtorg/gitmad/threads/activity/ConstraintLayoutLogin.java
@@ -0,0 +1,15 @@
+package edu.gatech.gtorg.gitmad.threads.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.gtorg.gitmad.threads.R;
+
+public class ConstraintLayoutLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout_login);
+    }
+}
