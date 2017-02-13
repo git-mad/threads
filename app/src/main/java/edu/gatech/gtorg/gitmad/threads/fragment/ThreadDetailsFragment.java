fed705caaecfecc0805062632354597911aa9b5e
Clayton Lawrence <ca.lawrence03@gmail.com>
2017-02-12 20:20:49 -0500

    Added advanced challenge for AsyncTasks

--- app/src/main/java/edu/gatech/gtorg/gitmad/threads/fragment/ThreadDetailsFragment.java
+++ app/src/main/java/edu/gatech/gtorg/gitmad/threads/fragment/ThreadDetailsFragment.java
@@ -2,15 +2,33 @@ package edu.gatech.gtorg.gitmad.threads.fragment;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import edu.gatech.gtorg.gitmad.threads.R;
 
@@ -20,11 +38,15 @@ public class ThreadDetailsFragment extends Fragment {
     private static final String KEY_THREAD_DETAILS = "thread details instance state key";
     private static final String EMPTY_STRING = "";
     private static final int SEND_REQUEST_CODE = 1;
+    private static final String URL = "http://grfx.cstv.com/schools/psu/graphics/auto/GT_Logo.jpg";
 
     private String threadName;
     private String threadDetails;
 
     private TextView threadDescriptionTextView;
+    private Button sendButton;
+    private Button downloadButton;
+    private ImageView imageView;
 
 
     public ThreadDetailsFragment() {
@@ -58,7 +80,9 @@ public class ThreadDetailsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_thread_details, container, false);
 
         threadDescriptionTextView = (TextView) rootView.findViewById(R.id.detailsTextView);
-        Button sendButton = (Button) rootView.findViewById(R.id.button_send);
+        sendButton = (Button) rootView.findViewById(R.id.button_send);
+        downloadButton = (Button) rootView.findViewById(R.id.button_download);
+        imageView = (ImageView) rootView.findViewById(R.id.download_view);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,6 +107,18 @@ public class ThreadDetailsFragment extends Fragment {
                 if (sendIntent.resolveActivity(getActivity().getPackageManager()) != null) {
                     startActivityForResult(chooser, SEND_REQUEST_CODE);
                 }
+            }
+        });
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* TODO 12 (Advanced)
+                 * Create a new instance of your async task and execute it here using the static URL
+                 * at the top of the file. If everything goes right, you'll have a familiar looking
+                 * logo in your image view.
+                 */
+                new ImageTask().execute(URL);
             }
         });
 
@@ -114,4 +150,72 @@ public class ThreadDetailsFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Toast.makeText(getContext(),"Successful onActivityResult!", Toast.LENGTH_SHORT).show();
     }
+
+    public Drawable getImage(String string_url) throws IOException {
+        URL url = new URL(string_url);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode != HttpsURLConnection.HTTP_OK) {
+                Log.i("error", "HTTP error code: " + responseCode);
+            }
+            InputStream input = connection.getInputStream();
+            BufferedInputStream buff = new BufferedInputStream(input);
+            Bitmap bitmap = BitmapFactory.decodeStream(buff);
+
+            if (input != null) {
+                input.close();
+            }
+
+            if (buff != null) {
+                buff.close();
+            }
+            return new BitmapDrawable(getResources(), bitmap);
+
+        } catch (MalformedURLException mue) {
+            Log.e("mue", mue.getMessage());
+
+        }  finally {
+            connection.disconnect();
+        }
+
+        return null;
+    }
+
+    /* TODO 11 (Advanced)
+     * The method getImage(<url>) is defined above to download an image at a given URL and return
+     * a drawable image that may be displayed for the user. We can't do the download on the main UI
+     * thread, so create an AsyncTask that runs getImage in the background and updates the ImageView
+     * upon completion. You'll need to override doInBackground and onPostExecute for this.
+     *
+     * Hint: When defining the AsyncTask, it has 3 type parameters - AsyncTask<1,2,3>
+     * 1. Type for the doInBackground var args
+     * 2. Type for onProgressUpdate var args
+     * 3. Type for onPostExecute which is also the return type from doInBackground
+     */
+    private class ImageTask extends AsyncTask<String, Void, Drawable> {
+
+        @Override
+        protected Drawable doInBackground(String... param) {
+            try {
+                return getImage(param[0]);
+            } catch (IOException ioe) {
+                Log.e("ioe", ioe.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Drawable image) {
+            updateImageView(image);
+        }
+
+    }
+
+    private void updateImageView(Drawable image) {
+        imageView.setImageDrawable(image);
+    }
+
 }
