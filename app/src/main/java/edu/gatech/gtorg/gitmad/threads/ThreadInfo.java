e65366fbe5ac435412fc32a325415a98020af922
Kevin Randrup <kevinrandrup@gmail.com>
2017-02-27 17:51:08 -0500

    Implemented Retrofit

--- app/src/main/java/edu/gatech/gtorg/gitmad/threads/ThreadInfo.java
+++ app/src/main/java/edu/gatech/gtorg/gitmad/threads/ThreadInfo.java
@@ -1,12 +1,20 @@
 package edu.gatech.gtorg.gitmad.threads;
 
 import android.content.Context;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 /**
  * Stores information about the Georiga Tech Thread program
@@ -37,10 +45,11 @@ public class ThreadInfo {
     }
 
     public static void loadThreadInformation(@NonNull final Context context, @Nullable final OnThreadLoadListener threadLoadListener) {
-        boolean requiresLoad = (orderedThreadNames == null || threadDetails == null);
+        final boolean requiresLoad = (orderedThreadNames == null || threadDetails == null);
         if (!requiresLoad) {
             return;
         }
+        /*
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -52,5 +61,46 @@ public class ThreadInfo {
                 }
             }
         }, 5000); // 5 second delay (delay in milliseconds)
+        */
+
+
+        service.getThreads().enqueue(new Callback<Map<String, String>>() {
+            @Override
+            public void onResponse(Call<Map<String, String>> call, Response<Map<String, String>> response) {
+                if (response.isSuccessful()) {
+                    Map<String, String> results = response.body();
+                    // Get the thread names and sort them
+                    orderedThreadNames = results.keySet().toArray(new String[0]);
+                    Arrays.sort(orderedThreadNames);
+                    // Then create thread details in the correct order
+                    threadDetails = new String[orderedThreadNames.length];
+                    for (int i = 0; i < orderedThreadNames.length; i += 1) {
+                        threadDetails[i] = results.get(orderedThreadNames[i]);
+                    }
+
+                    if (threadLoadListener != null) {
+                        threadLoadListener.onThreadInfoLoaded();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Map<String, String>> call, Throwable t) {
+                Log.e("ThreadInfo", "Request " + call.request().toString() + " failed with error " + t.getMessage());
+            }
+        });
     }
+
+    private static ThreadDataService service = new Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com/git-mad/threads/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ThreadDataService.class);
+
+
+    private interface ThreadDataService {
+        @GET("retrofit/ThreadData.json")
+        Call<Map<String, String>> getThreads();
+    }
+
 }
